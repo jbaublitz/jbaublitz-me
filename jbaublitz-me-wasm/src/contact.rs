4dd58a4655da8e08e8496a0c38ea65522fd779ea
John Baublitz <john.m.baublitz@gmail.com>
2019-07-01 12:24:26 -0400

    Start some graphics work in wasm

--- jbaublitz-me-wasm/src/contact.rs
+++ jbaublitz-me-wasm/src/contact.rs
@@ -1,22 +1,20 @@
 pub const CONTACT: &'static str = r#"
-<div id="center-panel-inner">
-    <div class="contact-type">
-        <p><b>Freenode IRC handle:</b></p>
-    </div>
-    <div class="contact-info">
-        <p>jbaublitzzz</p>
-    </div>
-    <div class="contact-type">
-        <p><b>Email:</b></p>
-    </div>
-    <div class="contact-info">
-        <p>john.m.baublitz@gmail.com</p>
-    </div>
-    <div class="contact-type">
-        <p><b>Github:</b></p>
-    </div>
-    <div class="contact-info">
-        <p><a href="https://github.com/jbaublitz">https://github.com/jbaublitz</a></p>
-    </div>
+<div class="contact-type">
+    <p><b>Freenode IRC handle:</b></p>
+</div>
+<div class="contact-info">
+    <p>jbaublitzzz</p>
+</div>
+<div class="contact-type">
+    <p><b>Email:</b></p>
+</div>
+<div class="contact-info">
+    <p>john.m.baublitz@gmail.com</p>
+</div>
+<div class="contact-type">
+    <p><b>Github:</b></p>
+</div>
+<div class="contact-info">
+    <p><a href="https://github.com/jbaublitz">https://github.com/jbaublitz</a></p>
 </div>
 "#;
--- jbaublitz-me-wasm/src/humans.rs
+++ jbaublitz-me-wasm/src/humans.rs
@@ -1,20 +1,18 @@
 pub const HUMANS: &'static str = r#"
-<div id="center-panel-inner">
-    <div class="body">
-        <div class="summary">
-            <p>We often forget about the <b>human</b> component of software in the industry.</p>
-        </div>
-        <p>In an effort to challenge the notion that software is only about the tech, these are the things I try to bring to my collaborations:</p>
+<div class="body">
+    <div class="summary">
+        <p>We often forget about the <b>human</b> component of software in the industry.</p>
     </div>
-    <ul class="left-list">
-        <li>Communicate online like you would face to face</li>
-        <li>Acknowledge that we're all still learning</li>
-        <li>Diversity matters and starts with individual acceptance of difference</li>
-        <li>When explaining, think back to when you were learning and explain it to yourself back then</li>
-        <li>Empathy is crucial and should be practiced mutually during interactions with collaborators</li>
-    </ul>
-    <div class="body">
-        <p>I work on a volunteer basis as part of a introductory systems education group at <a href="https://www.resilientcoders.org">Resilient Coders</a>. If you think diversity matters, click on the link and consider supporting an organization that's trying to make that happen in a real and actionable way!</p>
-    </div>
+    <p>In an effort to challenge the notion that software is only about the tech, these are the things I try to bring to my collaborations:</p>
+</div>
+<ul class="left-list">
+    <li>Communicate online like you would face to face</li>
+    <li>Acknowledge that we're all still learning</li>
+    <li>Diversity matters and starts with individual acceptance of difference</li>
+    <li>When explaining, think back to when you were learning and explain it to yourself back then</li>
+    <li>Empathy is crucial and should be practiced mutually during interactions with collaborators</li>
+</ul>
+<div class="body">
+    <p>I work on a volunteer basis as part of a introductory systems education group at <a href="https://www.resilientcoders.org">Resilient Coders</a>. If you think diversity matters, click on the link and consider supporting an organization that's trying to make that happen in a real and actionable way!</p>
 </div>
 "#;
