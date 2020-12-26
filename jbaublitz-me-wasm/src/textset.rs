c3cf7bd8949830f62d359bc572beb0ec8cba4344
John Baublitz <john.m.baublitz@gmail.com>
2020-12-26 07:51:21 -0500

    Remove graphics temporarily

--- jbaublitz-me-wasm/src/textset.rs
+++ jbaublitz-me-wasm/src/textset.rs
@@ -5,13 +5,16 @@ use web_sys::window;
 use super::JbaublitzError;
 
 pub fn set_text(text: &str) -> Result<(), Box<dyn Error>> {
-    let elem_op = window().and_then(|w| w.document())
+    let elem_op = window()
+        .and_then(|w| w.document())
         .and_then(|d| d.get_element_by_id("center-panel"));
     let elem_centerpanel = match elem_op {
         Some(e) => e,
         None => {
-            return Err(Box::new(JbaublitzError("No element with id=\"center-panel\" \
-                                                       found")));
+            return Err(Box::new(JbaublitzError(
+                "No element with id=\"center-panel\" \
+                                                       found",
+            )));
         }
     };
     elem_centerpanel.set_inner_html(text);
