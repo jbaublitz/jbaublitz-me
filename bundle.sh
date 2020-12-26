9a6edd64114b280bb2c6f0dabaafb417e8758e81
John Baublitz <john.m.baublitz@gmail.com>
2020-12-26 08:00:10 -0500

    Fix bundle.sh formatting

--- bundle.sh
+++ bundle.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp ./contact.css ./GitHub-Mark-Light-64px.png ./graphics.css ./humans.css ./index.css ./index.html ./template.css ./jbaublitz-me-wasm/pkg/
+cp ./contact.css \
+    ./GitHub-Mark-Light-64px.png \
+    ./humans.css \
+    ./index.css \
+    ./index.html \
+    ./template.css \
+    ./jbaublitz-me-wasm/pkg/
